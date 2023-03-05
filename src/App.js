c3dac8541418dfabbccbaa4db0b0c6f0536e79b3
jrfm <moesch.jonas@protonmail.com>
2023-03-05 15:08:00 +0100

    last save befor refractor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /* import logo from "./logo.svg"; */
 import React, { Component } from "react";
+import { ReactDOM } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Overview from "./components/Overview";
@@ -12,7 +13,10 @@ class App extends Component {
     this.onClickBtn = this.onClickBtn.bind(this);
 
     this.inputValue = {
-      tasksArray: [{ taskId: 0, taskName: "Clean House" }],
+      tasksArray: [
+        { taskId: 0, taskName: "Clean House" },
+        { taskId: 1, taskName: "Clean House" },
+      ],
     };
   }
 
@@ -27,15 +31,41 @@ class App extends Component {
       taskName: currTask.value,
     };
 
+    // push task object
     this.inputValue.tasksArray.push(currTaskObj);
     console.log(this.inputValue.tasksArray);
 
+    // delete overview node
+    const eleToDlt = document.getElementsByClassName("overview-container");
+    //console.log(eleToDlt);
+    eleToDlt[0].remove();
+
+    // // update overview
+    // const parent = document.getElementsByClassName("App");
+    // console.log(parent);
+
+    // const test = new Overview(this.inputValue.tasksArray);
+    // console.log(test);
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+
+    // <Overview arr={this.inputValue.tasksArray} />
+
+    // get din.classname = "app"
+    // delete last node
+    // add new overview component with new items
+
     // turn data into dom elements
-    const arrayItems = this.inputValue.tasksArray.map((ele) => (
-      <li key={ele.taskId}>ele</li>
-    ));
-    console.log(arrayItems);
-    return <ul>{arrayItems}</ul>;
+    // const arrayItems = this.inputValue.tasksArray.map((ele) => (
+    //   <li key={ele.taskId}>ele</li>
+    // ));
+    // console.log(arrayItems);
+    // return <ul>{arrayItems}</ul>;
   }
 
   render() {
@@ -43,7 +73,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <InputForm onButtonClicked={this.onClickBtn} />
-        <Overview />
+        <Overview arr={this.inputValue.tasksArray} />
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -8,10 +8,30 @@ class Overview extends Component {
   render() {
     return (
       <div className="overview-container">
-        <h2>Tasks List</h2>
+        {/* <ul>{console.log(this.props.arr)}</ul> */}
+        <ul>
+          {this.props.arr.map((ele) => {
+            return <li key={ele.taskId}>{ele.taskName}</li>;
+          })}
+        </ul>
       </div>
     );
   }
 }
 
 export default Overview;
+
+// {this.props.arr.map((ele) => (
+//   <li key={ele.taskId}>{ele}</li>
+// ))}
+
+// {this.props.tasksArray.map((ele) => (
+//   <li key={ele.taskId}>{ele}</li>
+// ))}
+
+// turn data into dom elements
+// const arrayItems = this.inputValue.tasksArray.map((ele) => (
+//   <li key={ele.taskId}>ele</li>
+// ));
+// console.log(arrayItems);
+// return <ul>{arrayItems}</ul>;
