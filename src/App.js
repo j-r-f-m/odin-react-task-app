60eed78f791c016cc7b343b4d3b8681c2f2bd2a7
jrfm <moesch.jonas@protonmail.com>
2023-03-05 11:16:16 +0100

    add dummy overview-component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Overview from "./components/Overview";
 
 class App extends Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <Overview />
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,1 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
+
+class Overview extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="overview-container">
+        <h2>Tasks</h2>
+      </div>
+    );
+  }
+}
+
+export default Overview;
